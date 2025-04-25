5ce3809fd297366bb16acc511205b8b162a7b1e5
Rohit Makwana <rmakwana060@gmail.com>
2025-04-25 23:30:35 +0530

    Frontend updated

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react"
-import "./header.css"
-import { nav } from "../../data/Data"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import "./header.css";
+import { nav } from "../../data/Data";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [navList, setNavList] = useState(false)
+  const [navList, setNavList] = useState(false);
 
   return (
     <>
       <header>
-        <div className='container flex'>
-          <div className='logo'>
-            <img src='./images/Banglewala_logo.png' alt='' />
+        <div className="container flex">
+          <div className="logo">
+            <img src="./images/Banglewala_logo-rb.png" alt="" />
           </div>
-          <div className='nav'>
+          <div className="nav">
             <ul className={navList ? "small" : "flex"}>
               {nav.map((list, index) => (
                 <li key={index}>
@@ -22,22 +22,25 @@ const Header = () => {
               ))}
             </ul>
           </div>
-          <div className='button flex'>
-            <h4>
-              My List
-            </h4>
-            <button className='btn1'>
-              <i className='fa fa-sign-out'></i> Sign In
+          <div className="button flex">
+            <button className="btn1">
+              <i className="fa fa-sign-in"></i> Sign In
             </button>
           </div>
 
-          <div className='toggle'>
-            <button onClick={() => setNavList(!navList)}>{navList ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}</button>
+          <div className="toggle">
+            <button onClick={() => setNavList(!navList)}>
+              {navList ? (
+                <i className="fa fa-times"></i>
+              ) : (
+                <i className="fa fa-bars"></i>
+              )}
+            </button>
           </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
